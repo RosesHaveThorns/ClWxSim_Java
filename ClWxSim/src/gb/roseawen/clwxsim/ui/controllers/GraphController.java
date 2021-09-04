0e5a223e246b0bb2ab9eee7fa49ed45cea59b594
Rose Brindle <rosebrindle@outlook.com>
2021-09-04 15:53:11 +0100

    moved to Maven Package system

--- ClWxSim/src/gb/roseawen/clwxsim/ui/controllers/GraphController.java
+++ ClWxSim/src/gb/roseawen/clwxsim/ui/controllers/GraphController.java
@@ -1,8 +1,16 @@
 package gb.roseawen.clwxsim.ui.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+
+import org.jetbrains.skija.*;
 
 public class GraphController extends SceneController {
     public ImageView mainGraph;
 
+    @FXML
+    public void reloadGraph(ActionEvent actionEvent) {
+
+    }
 }
